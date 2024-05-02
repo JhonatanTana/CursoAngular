8dd00c41eff1611dc1b4f083224f6dbf34ff9760
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-05-02 18:41:10 -0300

    Aula #34

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -9,9 +9,12 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-    allMoments:Moment[] = [];
-    moments:Moment[] = [];
-    baseApiUrl = environment.baseApiUrl
+  allMoments:Moment[] = [];
+  moments:Moment[] = [];
+  baseApiUrl = environment.baseApiUrl
+
+  faSearch = faSearch
+  searchTerm:string = ''
 
     constructor(
       private momentService:MomentService, //inicializa o serviço de momentos
@@ -31,6 +34,15 @@ export class HomeComponent {
       this.allMoments = items.data;
       this.moments = items.data;
     });
+  }
+
+  search(e: Event):void {
+    const target = e.target as HTMLInputElement
+    const value = target.value
+
+    this.moments = this.allMoments.filter (moment => {
+      return moment.title.toLowerCase().includes(value)
+    })
   }
 }
 
