c9d475a6a877483d5a5af4bee59efae6724cf7c7
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-05-04 01:03:40 -0300

    Aula #41

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,11 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import { Component,OnInit } from "@angular/core";
+import { FormGroup,FormControl, Validators, FormGroupDirective, } from "@angular/forms";
 import { MomentService } from "../../../service/moment.service";
-import { Moment } from "../../../Moment";
-import { Router, ActivatedRoute } from "@angular/router";
-import { environment } from "../../../../environments/environment";
-import { faEdit, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { MessagesService } from "../../../service/messages.service";
-
+import { Comment } from '../../../Comment';
+import { CommentService } from "../../../service/comment.service";
+import { ActivatedRoute } from "@angular/router";
+import { Moment } from "../../../Moment";
+import { environment } from "../../../../environments/environment";
+import { Router } from "@angular/router";
+import { faTimes,faEdit } from "@fortawesome/free-solid-svg-icons";
 
 
 @Component({
@@ -16,14 +19,18 @@ import { MessagesService } from "../../../service/messages.service";
 export class MomentComponent implements OnInit{
   moment?: Moment
   baseApiUrl = environment.baseApiUrl
+
   faTimes = faTimes
   faEdit = faEdit
+
+  commentForm!: FormGroup
 
   constructor(
     private momentService: MomentService,
     private route: ActivatedRoute,
     private messagesService: MessagesService,
     private router: Router,
+    private commentService: CommentService,
   ) {}
 
   ngOnInit():void {
@@ -32,12 +39,44 @@ export class MomentComponent implements OnInit{
     this.momentService
       .getMoment(id).subscribe(item =>
       (this.moment = item.data));
+
+    this.commentForm = new FormGroup({
+      text: new FormControl("", [Validators.required]),
+      username: new FormControl("", [Validators.required]),
+    })
   }
 
-  async removeHandler(id:number) {
+  get text() {
+    return this.commentForm.get("text")!
+  }
+
+  get username() {
+    return this.commentForm.get("username")!
+  }
+
+  async removeHandler(id:number) { //remove Momentos
     await this.momentService.removeMoment(id).subscribe()
 
     this.messagesService.add(" momento excluido com sucesso! ");
     this.router.navigate (["/"])
+  }
+
+  async onSubmit(formDirective: FormGroupDirective) {
+    if (this.commentForm.invalid) {
+      return;
+    }
+
+    const data: Comment = this.commentForm.value;
+    data.momentId = Number(this.moment!.id);
+
+    await this.commentService
+      .createComment(data)
+      .subscribe((comment) => this.moment!.comments!.push(comment.data));
+
+    this.messagesService.add(`Comentário adicionado!`);
+
+    //reseta o formulario
+    this.commentForm.reset();
+    formDirective.resetForm();
   }
 }
